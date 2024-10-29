a7bf253a4c869b13f7d52000af39bf1995793f3f
RafaelPA13 <rafaporann@gmail.com>
2024-10-29 17:30:26 -0300

    Config tailwind css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "color-brand": "#0EA5E9",
+        "color-darkest": "#0F172A",
+        "color-dark": "#37476C",
+        "color-medium": "#CBD5E1",
+        "color-light": "#E6ECF3",
+        "color-lightest": "#FFFFFF",
+      },
+      fontFamily: {
+        'inter': ["Inter", "sans-serif"],
+      },
+    },
   },
   plugins: [],
-}
-
+};
